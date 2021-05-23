180a9d8a10706f41b3c678d4d7e03815554b82b8
User <myemailid000001@gmail.com>
2021-05-23 15:22:16 +0900

    before commit restored

--- src/main/java/egovframework/sample/service/impl/SampleDAOJDBC.java
+++ src/main/java/egovframework/sample/service/impl/SampleDAOJDBC.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.List;test
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
