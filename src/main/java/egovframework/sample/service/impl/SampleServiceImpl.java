5b77dc0dbb566eff6f0f9c82c33760d6ddfff047
User <myemailid000001@gmail.com>
2021-05-24 21:22:20 +0900

    Separation of Concerns cannot completely separated into Crosscutting and Core on OOP

--- src/main/java/egovframework/sample/service/impl/SampleServiceImpl.java
+++ src/main/java/egovframework/sample/service/impl/SampleServiceImpl.java
@@ -9,37 +9,31 @@ import org.springframework.stereotype.Service;
 import egovframework.sample.service.SampleDAO;
 import egovframework.sample.service.SampleService;
 import egovframework.sample.service.SampleVO;
-import egovframework.sample.service.common.SampleAdvice;
+import egovframework.sample.service.common.AdvancedSampleAdvice;
 
 @Service("sampleService")
 public class SampleServiceImpl implements SampleService {
 	@Resource(name="daoJDBC")
 	private SampleDAO sampleDAO;
-	private SampleAdvice advice = new SampleAdvice();
 	
 	public void insertSample(SampleVO vo) throws Exception {
-		advice.beforeLogic();
 		sampleDAO.insertSample(vo);
 	}
 	
 	public void updateSample(SampleVO vo) throws Exception {
-		advice.beforeLogic();
 		sampleDAO.updateSample(vo);
 	}
 	
 	public void deleteSample(SampleVO vo) throws Exception {
-		advice.beforeLogic();
 		sampleDAO.deleteSample(vo);
 	}
 	
 
 	public SampleVO selectSample(SampleVO vo) throws Exception {
-		advice.beforeLogic();
 		return sampleDAO.selectSample(vo);
 	}
 	
 	public List<SampleVO> selectSampleList(SampleVO vo) throws Exception {
-		advice.beforeLogic();
 		return sampleDAO.selectSampleList(vo);
 	}
 }
