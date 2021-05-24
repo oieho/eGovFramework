dc35c52f77130e027e89fe6b630914ede4d52197
User <myemailid000001@gmail.com>
2021-05-24 21:37:36 +0900

    Because of AOP using context-aop.xml It has an diverse advantage. one is concise code, another is easily seeking for and the other is not using java source but .xml file

--- src/main/java/egovframework/sample/service/common/AdvancedSampleAdvice.java
+++ src/main/java/egovframework/sample/service/common/AdvancedSampleAdvice.java
@@ -1,7 +1,7 @@
 package egovframework.sample.service.common;
 
 public class AdvancedSampleAdvice {
-	public void AdvancedbeforeLogic(){
+	public void advancedBeforeLogic(){
 		System.out.println("[개선된 사전 처리] 비즈니스 로직 수행 전 동작");
 	}
 }
