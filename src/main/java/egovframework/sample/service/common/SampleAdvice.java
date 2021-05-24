5d7fcdaf09be55d14702f1631209a46c34219517
User <myemailid000001@gmail.com>
2021-05-24 22:18:27 +0900

    aop:after

--- src/main/java/egovframework/sample/service/common/SampleAdvice.java
+++ src/main/java/egovframework/sample/service/common/SampleAdvice.java
@@ -4,4 +4,8 @@ public class SampleAdvice {
 	public void beforeLogic(){
 		System.out.println("[사전 처리] 비즈니스 로직 수행 전 동작");
 	}
+	
+	public void afterLogic() {
+		System.out.println("[사후 처리] 비즈니스 로직 수행 후 동작");
+	}
 }
