f8e81d665e78b44e651e7682c9876518ebc1950e
User <oiehomail@gmail.com>
2021-05-25 16:58:49 +0900

    After-Returning,After-Throwing,Around with JoinPoint

--- src/main/java/egovframework/sample/service/common/SampleAdvice.java
+++ src/main/java/egovframework/sample/service/common/SampleAdvice.java
@@ -1,7 +1,13 @@
 package egovframework.sample.service.common;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
+
+import egovframework.sample.service.SampleVO;
 
 public class SampleAdvice {
 	public void beforeLogic(JoinPoint jp){
@@ -10,11 +16,17 @@ public class SampleAdvice {
 		System.out.println("[사전 처리] " + method + "() 메소드 ARGS 정보 : " + args[0].toString());
 	}
 	
-	public void afterReturningLogic() {
-		System.out.println("[사후 처리] 비즈니스 메소드 리턴 값을 받아서 동작");
-	}
-	public void afterThrowingLogic() {
-		System.out.println("[예외 처리] 비즈니스 로직 수행 중 예외 발생!");
+	public void afterThrowingLogic(JoinPoint jp, Exception exceptObj) {
+		String method = jp.getSignature().getName();
+		System.out.println("[예외 처리] " + method + "() 메소드 수행 중 예외 발생!");
+		
+		if(exceptObj instanceof IllegalArgumentException) {
+			System.out.println("부적절한 아규먼트 정보가 입력되었습니다.");
+		} else if(exceptObj instanceof SQLException) {
+			System.out.println("데이터베이스 연동에 문제가 발생했습니다.");
+		} else {
+			System.out.println("문제 발생!");
+		}
 	}
 	
 	public void afterLogic() {
@@ -22,9 +34,24 @@ public class SampleAdvice {
 	}
 
 	public Object aroundLogic(ProceedingJoinPoint pjp) throws Throwable {
-		System.out.println("[BEFORE]: 비즈니스 메소드 수행 전에 처리할 내용...");
-		Object returnObj = pjp.proceed();
-		System.out.println("[AFTER]: 비즈니스 메소드 수행 후에 처리할 내용...");
-		return returnObj;
+		String method = pjp.getSignature().getName();
+		
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		
+		Object obj = pjp.proceed();
+		
+		stopWatch.stop();
+		System.out.println(method + "() 메소드 수행에 걸린 시간 : " + stopWatch.getTotalTimeMillis() + "(ms)초");
+		return obj;
+	}
+	
+	public void afterReturningLogic(JoinPoint jp, Object returnObj) {
+		String method = jp.getSignature().getName();
+		System.out.println("[사후 처리] " + method + "() 메소드 리턴 값을 받아서 동작");
+		if(returnObj instanceof List) {
+			List<SampleVO> sampleList = (List<SampleVO>) returnObj;
+			System.out.println("검색된 데이터 : " + sampleList.size() + "건");
+		}
 	}
 }
