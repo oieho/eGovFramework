843ecdca9fe9ad2d1fd4785ae80202d3b6b93657
User <oiehomail@gmail.com>
2021-05-25 14:44:59 +0900

    Before Advice is provided Method of JoinPoint

--- src/main/java/egovframework/sample/service/common/SampleAdvice.java
+++ src/main/java/egovframework/sample/service/common/SampleAdvice.java
@@ -1,10 +1,13 @@
 package egovframework.sample.service.common;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 
 public class SampleAdvice {
-	public void beforeLogic(){
-		System.out.println("[사전 처리] 비즈니스 로직 수행 전 동작");
+	public void beforeLogic(JoinPoint jp){
+		String method = jp.getSignature().getName();
+		Object[] args = jp.getArgs();
+		System.out.println("[사전 처리] " + method + "() 메소드 ARGS 정보 : " + args[0].toString());
 	}
 	
 	public void afterReturningLogic() {
